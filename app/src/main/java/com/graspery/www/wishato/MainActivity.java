2fe05924eebe1dc99d7b5b389a431a21b2d491c8
Graspery <graspery@gmail.com>
2020-02-17 11:04:49 -0800

    First Commit

---
+++ app/src/main/java/com/graspery/www/wishato/MainActivity.java
@@ -0,0 +1,26 @@
+package com.graspery.www.wishato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "https://wishato.com/";
+
+        mWebView = findViewById(R.id.webview);
+        mWebView.getSettings().setLoadsImagesAutomatically(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mWebView.loadUrl(url);
+    }
+}
